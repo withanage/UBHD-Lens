da7510c6b84d60afb15be09360ecdb365d032a34
withanage <dulip.withanage@gmail.com>
2020-05-22 15:27:34 +0200

    Bug fix

--- src/custom_converter.js
+++ src/custom_converter.js
@@ -5,25 +5,25 @@ var LensConverter = require('lens/converter');
 var LensArticle = require("lens/article");
 var CustomNodeTypes = require("./nodes");
 
-var CustomConverter = function(options) {
+var CustomConverter = function (options) {
   LensConverter.call(this, options);
 };
 
-CustomConverter.Prototype = function() {
+CustomConverter.Prototype = function () {
 
-   this.test  = function (xmlDoc) {
-      // check  NLM jats elements
-      var article = xmlDoc.querySelector("article");
-      if (article !=null) {
-          return true;
-      }
-      return false;
-
+  this.test = function (xmlDoc) {
+    // check  NLM jats elements
+    var article = xmlDoc.querySelector("article");
+    if (article != null) {
+      return true;
     }
+    return false;
+
+  }
 
   // Override document factory so we can create a customized Lens article,
   // including overridden node types
-  this.createDocument = function() {
+  this.createDocument = function () {
     var doc = new LensArticle({
       nodeTypes: CustomNodeTypes
     });
@@ -31,18 +31,85 @@ CustomConverter.Prototype = function() {
     return doc;
   };
 
+  this.captionNew = function (state, caption) {
+    var doc = state.doc;
+    var captionNode = {
+      "id": state.nextId("caption"),
+      "source_id": caption.getAttribute("id"),
+      "type": "caption",
+      "title": "",
+      "children": []
+    };
+    // Titles can be annotated, thus delegate to paragraph
+    var title = caption.querySelector("title");
+    if (title) {
+      // Resolve title by delegating to the paragraph
+      var node = this.paragraph(state, title);
+      if (node) {
+        captionNode.title = node.id;
+      }
+    }
+    var children = [];
+    var paragraphs = caption.querySelectorAll("p");
+    _.each(paragraphs, function (p) {
+      // Only consider direct children
+      if (p.parentNode !== caption) return;
+      var node = this.paragraph(state, p);
+      if (node) children.push(node.id);
+    }, this);
+    captionNode.children = children;
+    doc.create(captionNode);
+    return captionNode;
+  };
+
+  this.sanitizeXML = function (xmlDoc) {
+    var paragraphs = xmlDoc.querySelectorAll("p");
+    for (var i = 0; i < paragraphs.length; i++) {
+      var paragraph = paragraphs[i];
+      var parentNode = paragraph.parentNode;
+      if (paragraph.innerHTML === "" && parentNode.tagName == "caption")
+        paragraph.innerHTML = ".";
+
+    }
+
+    var figures = xmlDoc.querySelectorAll("fig");
+
+    for (var i = 0; i < figures.length; i++) {
+      var figure = figures[i];
+      var hasCaption = false;
+      for (var j = 0; j < figure.children.length; j++) {
+        var child = figure.children[j];
+        if (child.tagName === "caption") {
+          hasCaption = true;
+        }
+      }
+      if (hasCaption===false) {
+        var caption = document.createElement("caption");
+        var paragraphElement = document.createElement("p");
+        var content = document.createTextNode(".");
+        paragraphElement.appendChild(content);
+        caption.appendChild(paragraphElement);
+        figure.appendChild(caption);
+
+      }
+    }
+
+    return xmlDoc;
+  };
+
+
   // Resolve figure urls
   // --------
   //
-/*
+  /*
 
-  this.enhanceFigure = function(state, node, element) {
-    var graphic = element.querySelector("graphic");
-    var url = graphic.getAttribute("xlink:href");
-    node.url = this.resolveURL(state, url);
-  };
+    this.enhanceFigure = function(state, node, element) {
+      var graphic = element.querySelector("graphic");
+      var url = graphic.getAttribute("xlink:href");
+      node.url = this.resolveURL(state, url);
+    };
 
-*/
+  */
 
   // Example url to JPG: http://cdn.elifesciences.org/elife-articles/00768/svg/elife00768f001.jpg
   /*
@@ -70,7 +137,7 @@ CustomConverter.Prototype = function() {
   */
 
   /**
-  this.enhanceVideo = function(state, node, element) {
+   this.enhanceVideo = function(state, node, element) {
     var href = element.getAttribute("xlink:href").split(".");
     var name = href[0];
     node.url = "http://api.elifesciences.org/v2/articles/"+state.doc.id+"/media/file/"+name+".mp4";
@@ -78,38 +145,9 @@ CustomConverter.Prototype = function() {
     node.url_webm = "http://api.elifesciences.org/v2/articles/"+state.doc.id+"/media/file//"+name+".webm";
     node.poster = "http://api.elifesciences.org/v2/articles/"+state.doc.id+"/media/file/"+name+".jpg";
   };
- **/
+   **/
 };
-this.caption = function (state, caption) {
-    var doc = state.doc;
-    var captionNode = {
-        "id": state.nextId("caption"),
-        "source_id": caption.getAttribute("id"),
-        "type": "caption",
-        "title": "",
-        "children": []
-    };
-    // Titles can be annotated, thus delegate to paragraph
-    var title = caption.querySelector("title");
-    if (title) {
-        // Resolve title by delegating to the paragraph
-        var node = this.paragraph(state, title);
-        if (node) {
-            captionNode.title = node.id;
-        }
-    }
-    var children = [];
-    var paragraphs = caption.querySelectorAll("p");
-    _.each(paragraphs, function (p) {
-        // Only consider direct children
-        if (p.parentNode !== caption) return;
-        var node = this.paragraph(state, p);
-        if (node) children.push(node.id);
-    }, this);
-    captionNode.children = children;
-    doc.create(captionNode);
-    return captionNode;
-};
+
 
 CustomConverter.Prototype.prototype = LensConverter.prototype;
 CustomConverter.prototype = new CustomConverter.Prototype();
