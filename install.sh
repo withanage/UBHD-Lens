c669fb246477cbf5305b83fa97810198ec809246
Withanage <dulip.withanage@tib.eu>
2022-04-28 18:16:10 +0200

    update

---
+++ install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/withanage/UBHD-Lens.git
+cd UBHD-Lens
+nvm use 6.14.4
+npm install gulp gulp-sass gulp-uglify browserify gulp-rename through2 path gulp-livereload rename st
+npm install
