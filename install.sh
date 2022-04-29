b55675c310004f30b6b4829967e633be361ec0d4
Withanage <dulip.withanage@tib.eu>
2022-04-29 19:28:40 +0200

    install script

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ cd UBHD-Lens
 nvm use 6.14.4
 npm install gulp gulp-sass gulp-uglify browserify gulp-rename through2 path gulp-livereload rename st
 npm install
+nvm install v6.17.1
+npm use 6.17.1
+npm install
+npm install gulp gulp-sass gulp-uglify browserify gulp-rename through2 path gulp-livereload rename st
